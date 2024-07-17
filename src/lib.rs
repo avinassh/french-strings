e04b5ec5b85432eecd66c0ca456119e1216d523c
Avinash Sajjanshetty <opensource@avi.im>
2024-07-17 16:06:20 +0530

    steal code from Marin

---
+++ src/lib.rs
@@ -0,0 +1,51 @@
+use std::mem::size_of;
+
+pub struct GermanString([u8; size_of::<u128>()]);
+
+impl GermanString {
+    pub fn new() -> Self {
+        Self([0; size_of::<u128>()])
+    }
+
+    #[inline]
+    pub fn len(&self) -> usize {
+        u32::from_ne_bytes(self.0[..size_of::<u32>()].try_into().unwrap()) as usize
+    }
+
+    pub fn push_str(&mut self, s: &str) {
+        let new_len = s.len() + self.len();
+        if new_len <= 12 {
+            let offset = size_of::<u32>() + self.len();
+            self.0[offset..offset + s.len()].copy_from_slice(s.as_bytes());
+            self.0[..size_of::<u32>()].copy_from_slice(&(new_len as u32).to_ne_bytes());
+        } else {
+            todo!()
+        }
+    }
+}
+
+impl AsRef<str> for GermanString {
+    fn as_ref(&self) -> &str {
+        if self.len() <= 12 {
+            let offset = size_of::<u32>();
+            unsafe {
+                std::str::from_utf8_unchecked(&self.0[offset..offset + self.len()])
+            }
+        } else {
+            todo!()
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn small() {
+        let mut s = GermanString::new();
+        s.push_str("hello");
+        assert_eq!(s.as_ref(), "hello");
+    }
+}
+
